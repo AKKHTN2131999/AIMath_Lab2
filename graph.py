1419f16c836d07dacc13d780e79241440c417294
Kiet Hoang <hoanganhkiet22121999@gmail.com>
2023-02-26 18:06:19 +0700

    complete project

--- graph.py
+++ graph.py
@@ -1,21 +1,24 @@
 from vertex import Vertex
 from typing import Any, Text, List
 
+from queue import Queue
+
+
 class Graph:
     def __init__(self):
         self.vertList = {}
         self.numVertices = 0
-        # self.time = 0
 
 
     def addVertex(self,key):
         self.numVertices = self.numVertices + 1
         newVertex = Vertex(key)
         self.vertList[key] = newVertex
+
         return newVertex
 
 
-    def getVertex(self,n):
+    def getVertex(self, n):
         return self.vertList[n] if n in self.vertList else None
 
 
@@ -23,15 +26,13 @@ class Graph:
         return n in self.vertList
 
 
-    def addEdge(
-        self,
-        f: Any,
-        t: Any,
-        weight: int =0):
+    def addEdge(self, f: Any, t: Any, weight: int =0):
         if f not in self.vertList:
             nv = self.addVertex(f)
+
         if t not in self.vertList:
             nv = self.addVertex(t)
+
         self.vertList[f].addNeighbor(self.vertList[t], weight)
 
 
@@ -42,73 +43,79 @@ class Graph:
     def __iter__(self):
         return iter(self.vertList.values())
 
+
     def bfs(self, start: Vertex):
-        vertQueue = self._extracted_from_dfs_2(start)
-        while vertQueue:
-            currentVert: Vertex = vertQueue.pop(0)
-            for nbr in currentVert.getConnections():
-                nbr: Vertex = nbr
-                if (nbr.getColor() == 'white'):
-                    self._extracted_from_dfs_10(nbr, currentVert, vertQueue)
-            currentVert.setColor('black')
-
-    def dfs(self, start: Vertex):
-        vertStack = self._extracted_from_dfs_2(start)
-        step = 0
-        while vertStack:
-            currentVert: Vertex = vertStack.pop(-1)
-            step+=1
-            for nbr in currentVert.getConnections():
-                nbr: Vertex = nbr
-                nbr.setDiscovery(step)
-                if (nbr.getColor() == 'white'):
-                    self._extracted_from_dfs_10(nbr, currentVert, vertStack)
-            currentVert.setColor('black')
-            
-            currentVert.setFinish(step)
-            step+=1
-
-    # TODO Rename this here and in `bfs` and `dfs`
-    def _extracted_from_dfs_2(self, start):
         start.setDistance(0)
         start.setPred(None)
-        return [start]
 
-    # TODO Rename this here and in `bfs` and `dfs`
-    def _extracted_from_dfs_10(self, nbr, currentVert, arg2):
-        nbr.setColor('gray')
-        nbr.setDistance(currentVert.getDistance() + 1)
-        nbr.setPred(currentVert)
-        arg2.append(nbr)
+        vertQueue = Queue()
+        vertQueue.put(start)
+
+        while vertQueue.qsize() > 0:
+            currentVert = vertQueue.get()
+
+            for nbr in currentVert.getConnections():
+                if nbr.getColor() == 'white':
+                    nbr.setColor('gray')
+                    nbr.setDistance(currentVert.getDistance() + 1)
+                    nbr.setPred(currentVert)
+
+                    vertQueue.put(nbr)
+
+            currentVert.setColor('black')
 
 
-# #Build word graph 
-# def buildGraph(wList):
-#     d = {}
-#     g = Graph()
-#     #phân hoạch các từ cùng độ dài chỉ khác nhau 1 ký tự
-#     for line in wList: #lấy từng từ trong từ điển
-#         word = line[:-1]
-#         for i in range(len(word)):
-#             bucket = f'{word[:i]}_{word[i + 1:]}'
-#             if bucket in d:
-#                 d[bucket].append(word)
-#             else:
-#                 d[bucket] = [word]
-#     #thêm các đỉnh và các cạnh cho các từng trong cùng bucket
-#     for bucket in d:
-#         for word1 in d[bucket]:
-#             for word2 in d[bucket]:
-#                 if word1 != word2:
-#                     g.addEdge(word1,word2)
-#     return g
+    def dfs(self):
+        time = 0
 
-def traverse(y:Vertex):
+        for aVertex in self:
+            aVertex.setColor('white')
+            aVertex.setPred(None)
+
+        for aVertex in self:
+            if aVertex.getColor() == 'white':
+                self.dfsvisit(aVertex, time)
+    
+
+    def dfsvisit(self, startVertex, time):
+        startVertex.setColor('gray')
+        time += 1
+        startVertex.setDiscovery(time)
+
+        for nextVertex in startVertex.getConnections():
+            if nextVertex.getColor() == 'white':
+                nextVertex.setPred(startVertex)
+                self.dfsvisit(nextVertex, time)
+
+        startVertex.setColor('black')
+        time += 1
+        startVertex.setFinish(time)
+
+
+    def traverse_dfs(self):
+        for vertex in self:
+            key = vertex.getId()
+            pred = 'None' if vertex.getPred() is None else vertex.getPred().getId()
+            discovery = vertex.getDiscovery()
+            finish = vertex.getFinish()
+
+            print("key: {}, pred: {}, discovery: {}, finish: {}".format(key, pred, discovery, finish))
+
+
+    def traverse_bfs(self):
+        for vertex in self:
+            key = vertex.getId()
+            pred = 'None' if vertex.getPred() is None else vertex.getPred().getId()
+            distance = vertex.getDistance()
+
+            print("key: {}, pred: {}, distance: {}".format(key, pred, distance))
+
+
+def traverse(y: Vertex):
     x = y
     while (x.getPred()):
         print(x.getId())
         x = x.getPred()
-        print(x.__dict__)
     print(x.getId())
 
 
@@ -116,19 +123,36 @@ if __name__ == '__main__':
     g = Graph()
     for i in range(9):
         g.addVertex(i)
-    g.addEdge(0,1)
-    g.addEdge(0,4)
-    g.addEdge(1,2)
-    g.addEdge(1,3)
-    g.addEdge(2,5)
-    g.addEdge(3,6)
-    # g.addEdge(3,5)
-    g.addEdge(6,7)
-    g.addEdge(6,8)
+    
+    g.addEdge(0, 1)
+    g.addEdge(1, 0)
+
+    g.addEdge(0, 4)
+    g.addEdge(4, 0)
+
+    g.addEdge(1, 2)
+    g.addEdge(2, 1)
+
+    g.addEdge(1, 3)
+    g.addEdge(3, 1)
+
+    g.addEdge(2, 5)
+    g.addEdge(5, 2)
+
+    g.addEdge(3, 6)
+    g.addEdge(6, 3)
+
+    g.addEdge(6, 7)
+    g.addEdge(7, 6)
+
+    g.addEdge(6, 8)
+    g.addEdge(8, 6)
+
+
     for v in g:
         for w in v.getConnections():
             print(f"( {v.getId()} , {w.getId()} )")
 
-    g.dfs(g.getVertex(0))
-    traverse(g.getVertex(6))
+    g.dfs()
+    g.traverse_dfs()
 
--- vertex.py
+++ vertex.py
@@ -1,41 +1,16 @@
 class Vertex:
-    def __init__(
-        self,
-        key,
-        ):
+    def __init__(self, key):
         self.id = key         
         self.connectedTo = {}
-        self.discovery = 0
+
         self.distance = 0
         self.pred = None
         self.color = "white"
+
+        self.discovery = 0
         self.finish = 0
 
     
-    def getDistance(self):
-        return self.distance
-
-    def getPred(self):
-        return self.pred
-
-    def getColor(self):
-        return self.color
-
-    def setDistance(self, distance: int):
-        self.distance = distance
-
-    def setPred(self, pred ):
-        self.pred = pred
-
-    def setColor(self, color: str):
-        self.color = color
-
-    def setDiscovery(self, discovery):
-        self.discovery = discovery
-
-    def setFinish(self, finish):
-        self.finish = finish
-
     def addNeighbor(self,nbr,weight=0):
         self.connectedTo[nbr] = weight
 
@@ -55,4 +30,43 @@ class Vertex:
     def getWeight(self,nbr):
         return self.connectedTo[nbr]
 
+    
+    def getDistance(self):
+        return self.distance
+
+
+    def getPred(self):
+        return self.pred
+
+
+    def getColor(self):
+        return self.color
+
+
+    def setDistance(self, distance: int):
+        self.distance = distance
+
+
+    def setPred(self, pred ):
+        self.pred = pred
+
+
+    def setColor(self, color: str):
+        self.color = color
+
+
+    def getDiscovery(self):
+        return self.discovery
+
+
+    def getFinish(self):
+        return self.finish
+
+
+    def setDiscovery(self, discovery):
+        self.discovery = discovery
+
+
+    def setFinish(self, finish):
+        self.finish = finish
 
